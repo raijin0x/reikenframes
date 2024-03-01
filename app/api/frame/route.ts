ee327f21320fcaf912e848acb9c4e9caa3eae4a5
raijin0x <139423310+raijin0x@users.noreply.github.com>
2024-03-01 19:46:11 +0700

    Update route.ts

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -22,7 +22,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   return new NextResponse(`<!DOCTYPE html><html><head>
     <title>This is frame ${id}</title>
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/Qme4FXhoxHHfyzTfRxSpASbMF8kajLEPkRQWhwWu9pkUjm/${id}.png" />
+    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/bafybeigds24255joxcl3wtv4ebeh5jv6fbt3hjb3ozbkisatbxvjn6hejy/${id}.jpg" />
     <meta property="fc:frame:button:1" content="Next Page" />
     <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${nextId}" />
   </head></html>`);
